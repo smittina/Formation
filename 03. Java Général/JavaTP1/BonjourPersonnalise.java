a9fad37eb00744b19f9ab083dd1915aa0aec8eb2
Melodie <meisselmelodie@gmail.com>
2019-05-02 11:32:52 +0200

    Commit des différents dossiers

---
+++ 03. Java Général/JavaTP1/BonjourPersonnalise.java
@@ -0,0 +1,5 @@
+public class BonjourPersonnalise{
+  public static void main(String args[]){
+    System.out.println("Bonjour "+args[0]);
+  }
+}
---
+++ 03. Java Général/JavaTP1/org/diginamic/Bonjour.java
@@ -0,0 +1,7 @@
+package org.diginamic;
+
+public class Bonjour{
+  public static void main(String args[]){
+    System.out.println("Bonjour tout le monde !");
+  }
+}
